f94fc739942e927e71a7a67197fab203dbee89b7
Jerry Gerou <saragerou@gmail.com>
2018-01-18 15:01:19 -0600

    Initial session setting up classes and making tests

--- blackjack.py
+++ blackjack.py
@@ -1,2 +1,40 @@
+from collections import namedtuple
+
+class Card(namedtuple('Card', ['rank', 'suit'])):
+    """
+    A card class with rank and suit.
+    """
+    __slots__ = ()
+    """
+    Using namedtupleand slots allows us to not use __init__. Like a constructor function so that we're not calling the Card class 52 times.
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+    """
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.rank}, {self.suit})"
+
+class Deck:
+
+    ranks = [str(i) for i in range(2, 11)] + list('JQKA')
+    suits = ['heart', 'diamond', 'spade', 'club']
+
+    def __init__(self):
+        """
+        self.cards = []
+        for suit in self.suits:
+            for rank in self.ranks:
+                self.cards.append(Card(rank, suit))
+        """
+        self.cards = [Card(rank, suit)  for suit in self. suits
+                                        for rank in self. ranks]
+
+    def __len__(self):
+        return len(self.cards)
+
+    def __getitem__(self, index):
+        return self.cards[index]
+
 if __name__ == "__main__":
     print("Hello World")
--- test_blackjack.py
+++ test_blackjack.py
@@ -1,3 +1,22 @@
+from blackjack import Card, Deck
 
-def test_the_truth():
-    assert True is True
+def test_card_has_rank_and_value():
+    card = Card(2, 'heart')
+    assert card.rank == 2
+    assert card.suit == 'heart'
+
+def test_deck_has_fifty_two_cards():
+    deck = Deck()
+    assert len(deck) == 52
+
+def test_deck_has_four_of_everything():
+    ranks = [str(i) for i in range(2, 11)] + list('JQKA')
+    rank_set = set(ranks)
+    deck = Deck()
+    hearts = [card for card in deck if card.suit == 'heart']
+    spades = [card for card in deck if card.suit == 'spade']
+    diamonds = [card for card in deck if card.suit == 'diamond']
+    clubs = [card for card in deck if card.suit == 'club']
+    assert all(
+        set([c.rank for c in cards]) == rank_set
+        for cards in [hearts, spades, diamonds, clubs])
